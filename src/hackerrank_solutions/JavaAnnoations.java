daab226f072d0e60b0a7a6e796aed099f3108fc1
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2020-05-09 11:06:46 +0200

    -solve java annotations

--- src/hackerrank_solutions/JavaAnnoations.java
+++ src/hackerrank_solutions/JavaAnnoations.java
@@ -12,18 +12,18 @@ import java.util.Scanner;
 @Retention(RetentionPolicy.RUNTIME)
 @interface FamilyBudget {
     String userRole() default "GUEST";
-	//~~Complete the interface~~
+	int budgetLimit() default 0;
 }
 
 class FamilyMember {
-	//~~Complete this line~~
+	@FamilyBudget(userRole = "SENIOR", budgetLimit = 100)
     public void seniorMember(int budget, int moneySpend) {
         System.out.println("Senior Member");
         System.out.println("Spend: " + moneySpend);
         System.out.println("Budget Left: " + (budget - moneySpend));
     }
 
-	//~~Complete this line~~
+    @FamilyBudget(userRole = "JUNIOR", budgetLimit = 50)
     public void juniorUser(int budget, int moneySpend) {
         System.out.println("Junior Member");
         System.out.println("Spend: " + moneySpend);
@@ -44,17 +44,16 @@ public class JavaAnnoations {
                 Method[] methods = annotatedClass.getMethods();
                 for (Method method : methods) {
                     if (method.isAnnotationPresent(FamilyBudget.class)) {
-                        FamilyBudget family = method
-                                .getAnnotation(FamilyBudget.class);
+                        FamilyBudget family = method.getAnnotation(FamilyBudget.class);
                         String userRole = family.userRole();
-                        //int budgetLimit =
+                        int budgetLimit = family.budgetLimit();
                         if (userRole.equals(role)) {
-                          /*  if( ){
+                            if(spend <=  budgetLimit){
                                 method.invoke(FamilyMember.class.newInstance(),
                                         budgetLimit, spend);
                             }else{
                                 System.out.println("Budget Limit Over");
-                            }*/
+                            }
                         }
                     }
                 }
