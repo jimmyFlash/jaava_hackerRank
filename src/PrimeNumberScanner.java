fa08b7c2f86ee08318da5231239b441b243ee593
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2021-02-18 12:28:43 +0200

    - fix PrimeNumberScanner error in calculation of last number in range

--- src/PrimeNumberScanner.java
+++ src/PrimeNumberScanner.java
@@ -11,27 +11,25 @@ public class PrimeNumberScanner {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int maxRange = sc.nextInt(); // read the maxRange of numbers
-        boolean[] isPrime = new boolean[maxRange + 1]; // create and array of booleans from 0 - range + 1
-        Arrays.fill(isPrime, true); // fill array with with values of true
-        // set the 1st and second indices to false
-        isPrime[0] = false; // 0 not prime
-        isPrime[1] = false; // 1 not prime
+        int maxRange = sc.nextInt();
+        boolean[] isPrime = new boolean[maxRange + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
 
-        // arraylist to store prime numbers
         ArrayList<Integer> primesFound = new ArrayList<>();
 
-        for (int i = 2; i <= maxRange; i++) { // start loop from 2
+        for (int i = 2; i <= maxRange; i++) {
 
             System.out.println("checking i= " + i);
-            if (isPrime[i]) { // check prime flag
-                primesFound.add(i); // add to primes arraylist
-                int numSquare = i * i; // get the square root of the current number
-                // as long as the numSquare is less than the max range
-                while(numSquare < maxRange){
-                    // the square of the value means that 3*3 = 9 so 9 is divisible by 3 making it not prime number
-                    isPrime[numSquare] = false; // mark corresponding index in booleans array with false
-                    numSquare += i; // increment the number by current iteration value
+            if (isPrime[i]) {
+                primesFound.add(i);
+                int numSquare = i * i;
+
+                while(numSquare <= maxRange){
+
+                    isPrime[numSquare] = false;
+                    numSquare += i;
                 }
             }
         }
